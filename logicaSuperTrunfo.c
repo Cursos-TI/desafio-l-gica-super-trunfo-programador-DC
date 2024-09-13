8818145d5a0508854ceaabf6dfa6ae9c2f8fb114
programador-DC <diogoacardoso01@gmail.com>
2024-09-13 03:06:55 +0000

    super trunfo, codigo de decisão

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -177,33 +177,7 @@ int main(){
 
         printf("Carta 1 ganhou %d\n", Vitoriacarta1);
         printf("Carta 2 ganhou %d\n", Vitoriacarta2);
-
     
-
-
-
-
-    
-    
-
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
     return 0;
 }
